0f4b25cbbdc35b67db8cecee4fdecd0248c8102a
Pathivada sathwik <sathwikp06@gmail.com>
2026-07-13 12:46:15 +0530

    modification

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
    <div className='flex flex-row bg-blue-500 mx-auto fixed bottom-0 justify-center w-[100%] align-middle'>
-     Created with <span className='mt-1 ml-2 mr-2'><img src="love.svg" height={10} width={20} alt="" /></span><b>-Madhava Reddy</b>
+     Created with <span className='mt-1 ml-2 mr-2'><img src="love.svg" height={10} width={20} alt="" /></span><b>-pathivada sathwik</b>
    </div>
 
 
